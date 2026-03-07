652c33a440447db9aac8e1a5c788ed1fa0f0c728
Jishnumnair <jishnum@karunya.edu.in>
2026-03-07 12:59:00 +0530

    completed exercises 1.1-1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -23,31 +23,30 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <p>
-      Number of exercises{" "}
+      Number of exercises{' '}
       {props.parts[0].exercises +
         props.parts[1].exercises +
         props.parts[2].exercises}
     </p>
   )
 }
-
 const App = () => {
   const course = {
-    name: "Half Stack application development",
+    name: 'Half Stack application development',
     parts: [
       {
-        name: "Fundamentals of React",
-        exercises: 10,
+        name: 'Fundamentals of React',
+        exercises: 10
       },
       {
-        name: "Using props to pass data",
-        exercises: 7,
+        name: 'Using props to pass data',
+        exercises: 7
       },
       {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
   return (
